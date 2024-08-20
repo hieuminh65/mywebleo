1415f0f9fce5dcf686d051ae11baca4a50d9896e
hieuminh65 <hieuminhnguyen@usf.edu>
2024-08-19 23:11:54 -0400

    feat: Add abstract shape component to AnimatedBlock

--- src/app/about/_components/AnimatedBlock.tsx
+++ src/app/about/_components/AnimatedBlock.tsx
@@ -3,12 +3,26 @@
 import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import styles from "./styles/AbstractShape.module.css";
+
+const AbstractShape: React.FC<{ shapeType: string; bgColor: string }> = ({
+  shapeType,
+  bgColor,
+}) => {
+  const shapeClass = styles[shapeType] || "";
+
+  return (
+    <div
+      className={`${shapeClass} w-24 h-24 ${bgColor} flex-shrink-0 flex justify-center items-center`}
+    ></div>
+  );
+};
 
 const AnimatedBlock: React.FC<{
   text: string;
-  imgSrc: string;
+  shapeType: string;
   bgColor: string;
-}> = ({ text, imgSrc, bgColor }) => {
+}> = ({ text, shapeType, bgColor }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
     triggerOnce: true,
@@ -33,13 +47,8 @@ const AnimatedBlock: React.FC<{
       animate={controls}
       className={`flex items-center space-x-4 mb-16 p-6 rounded-lg ${bgColor} mx-auto max-w-4xl transition-transform transition-shadow duration-300 hover:shadow-lg hover:scale-105 hover:bg-opacity-90`}
     >
-      <motion.img
-        src={imgSrc}
-        alt={text}
-        className="w-24 h-24 rounded-full object-cover"
-        whileHover={{ scale: 1.1 }}
-      />
-      <h2 className="text-xl font-medium text-off-black">{text}</h2>
+      <AbstractShape shapeType={shapeType} bgColor="bg-white" />
+      <h2 className="text-lg font-medium text-off-black flex-grow">{text}</h2>
     </motion.div>
   );
 };
@@ -47,53 +56,53 @@ const AnimatedBlock: React.FC<{
 const AnimatedBlocksPage: React.FC = () => {
   const blocks = [
     {
-      text: "Hi, I&apos;m Leo, but my real name is Hieu Minh Nguyen.",
-      imgSrc: "/images/avatar.png",
+      text: "Hi, I'm Leo, but my real name is Hieu Minh Nguyen.",
+      shapeType: "circle",
       bgColor: "bg-[#f8d7da]", // Soft Blush Pink
     },
     {
-      text: "I&apos;m an international student from Ha Noi, Vietnam.",
-      imgSrc: "/images/avatar.png",
+      text: "I'm an international student from Ha Noi, Vietnam.",
+      shapeType: "square",
       bgColor: "bg-[#cce3de]", // Light Sage Green
     },
     {
-      text: "I&apos;m currently an undergraduate student at the University of South Florida.",
-      imgSrc: "/images/usf.jpg",
+      text: "I'm currently an undergraduate student at the University of South Florida.",
+      shapeType: "hexagon",
       bgColor: "bg-[#e6e6fa]", // Pale Lavender
     },
     {
-      text: "I&apos;m pursuing a degree in Computer Science in the vibrant city of Tampa, Florida.",
-      imgSrc: "/images/tampa.jpg",
+      text: "I'm pursuing a degree in Computer Science in the vibrant city of Tampa, Florida.",
+      shapeType: "triangle",
       bgColor: "bg-[#b3cde0]", // Misty Blue
     },
     {
       text: "My journey in tech started with a fascination for problem-solving and building innovative software solutions.",
-      imgSrc: "/images/coding.jpg",
+      shapeType: "circle",
       bgColor: "bg-[#ffe5b4]", // Warm Peach
     },
     {
-      text: "Outside of the classroom, you&apos;ll find me on the soccer field, where I channel my love for the game.",
-      imgSrc: "/images/soccer.jpg",
+      text: "Outside of the classroom, you'll find me on the soccer field, where I channel my love for the game.",
+      shapeType: "hexagon",
       bgColor: "bg-[#cce3de]", // Light Sage Green
     },
     {
-      text: "I&apos;m a huge fan of Messi; his journey inspires me to push my limits in everything I do.",
-      imgSrc: "/images/messi.jpg",
+      text: "I'm a huge fan of Messi; his journey inspires me to push my limits in everything I do.",
+      shapeType: "square",
       bgColor: "bg-[#e6e6fa]", // Pale Lavender
     },
     {
-      text: "I&apos;m passionate about technology, especially in the areas of AI and software development.",
-      imgSrc: "/images/ai.jpg",
+      text: "I'm passionate about technology, especially in the areas of AI and software development.",
+      shapeType: "triangle",
       bgColor: "bg-[#f8d7da]", // Soft Blush Pink
     },
     {
-      text: "My goal is to create impactful software that can improve people&apos;s lives.",
-      imgSrc: "/images/software.jpg",
+      text: "My goal is to create impactful software that can improve people's lives.",
+      shapeType: "circle",
       bgColor: "bg-[#b3cde0]", // Misty Blue
     },
     {
-      text: "Thank you for joining me on this journey. Let&apos;s connect and create something amazing together!",
-      imgSrc: "/images/connect.jpg",
+      text: "Thank you for joining me on this journey. Let's connect and create something amazing together!",
+      shapeType: "hexagon",
       bgColor: "bg-[#ffe5b4]", // Warm Peach
     },
   ];
@@ -104,7 +113,7 @@ const AnimatedBlocksPage: React.FC = () => {
         <AnimatedBlock
           key={index}
           text={block.text}
-          imgSrc={block.imgSrc}
+          shapeType={block.shapeType}
           bgColor={block.bgColor}
         />
       ))}
