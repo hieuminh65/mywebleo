37fd87d97e5280961ed0a7d9aa6a1dd265c66ba4
hieuminh65 <hieuminhnguyen@usf.edu>
2024-08-21 17:48:38 -0400

    refactor: Update ContactInformation component to improve layout and mobile responsive

--- src/app/contact/_components/ContactInformation.tsx
+++ src/app/contact/_components/ContactInformation.tsx
@@ -7,24 +7,25 @@ const ContactInfo: React.FC = () => {
     <section className="bg-white py-20">
       <div className="container mx-auto px-6 text-center">
         <motion.div
-          className="mb-8"
+          className="mb-8 flex flex-col items-center justify-center"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
         >
-          <EnvelopeOpenIcon className="text-blue-500 w-10 h-10 mx-auto" />
-          <h2 className="text-3xl font-semibold text-off-black mt-4">
+          <EnvelopeOpenIcon className="text-blue-500 w-10 h-10" />
+          <h2 className="text-xl md:text-3xl font-semibold text-off-black mt-4">
             Gmail: hieung.tech@gmail.com
           </h2>
         </motion.div>
 
         <motion.div
+          className="flex flex-col items-center justify-center"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
         >
-          <LinkedInLogoIcon className="text-blue-600 w-10 h-10 mx-auto" />
-          <h2 className="text-3xl font-semibold text-off-black mt-4">
+          <LinkedInLogoIcon className="text-blue-600 w-10 h-10" />
+          <h2 className="text-xl md:text-3xl font-semibold text-off-black mt-4">
             LinkedIn: linkedin.com/in/hieunguyen65/
           </h2>
         </motion.div>
