7f3204d5374ddc0ae79cf42354fb07ab082d1f49
hieuminh65 <hieuminhnguyen@usf.edu>
2024-11-25 17:46:54 -0500

    add taiwindcss motion set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-motion")],
 } satisfies Config;
 
 export default config;
